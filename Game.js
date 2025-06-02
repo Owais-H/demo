aad48d4560e923a78ed5c501dd316d39144af07d
Owais-H <93500100+Owais-H@users.noreply.github.com>
2025-06-02 09:57:16 -0600

    game file
    uploading my game code javascript file

---
+++ Game.js
@@ -0,0 +1,10 @@
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowUp':
+    case 'ArrowDown':
+    case 'ArrowLeft':
+    case 'ArrowRight':
+      moveFrog(e.key);
+      break;
+  }
+});
